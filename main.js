c1cb4df3d1604925410f16b5c12e9a8c552c61ee
DostonbekZaripov <dastandev0110@gmail.com>
2024-12-06 01:44:53 +0500

    js bajarildi

---
+++ main.js
@@ -0,0 +1,75 @@
+// ForEach
+// Array.prototype.myForEach = function(cb){
+//     for(let i =0; i<this.length; i++){
+//         cb(this[i], i, this)
+//     }
+// }
+// users.myForEach((value,index,arr)=>{
+//     console.log(value, index,arr)
+// })
+
+let users = [
+    {
+        id:1,
+        username:"Doston",
+        password:"Doston123"
+    },
+    {
+        id:2,
+        username:"Farxod",
+        password:"Farxod123"
+    },
+    {
+        id:3,
+        username:"Dilfuza",
+        password:"Dilfuza123"
+    }
+]
+
+
+
+// Map
+// let newArr = [];
+// Array.prototype.myMap = function(cb){
+//     for(let i = 0; i<this.length; i++){
+//         if(i in this){
+//             cb(this[i],i, this)
+//         }
+//     }
+// }
+// users.myMap((value, index, arr)=>{
+//     console.log(value,index, arr)
+// })
+
+
+// Filter
+// numArr = [1,2,3,4,5,6]
+// newArr = [ ]
+
+// Array.prototype.myFilter = function(cb){
+//     for(let i= 0; i<this.length;i++){
+//         if(numArr[i] % 2 == 0){
+//            cb(newArr.push(this[i])) 
+//         }
+//     }
+// }
+// numArr.myFilter(()=>{
+//     console.log(newArr)
+// })
+
+
+// numArr = [1,2,3,4,5,6]
+// newArr = [ ]
+
+// Array.prototype.mySome = function(cb){
+//     for(let i= 0; i<this.length;i++){
+//         if(numArr[i] == true){
+//            cb(newArr.push(true)) 
+//         }
+//     }
+// }
+
+// numArr.mySome(()=>{
+//     console.log(newArr)
+// })
+
